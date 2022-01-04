8fab88d40fb2520409ac3a26c4dba930cc0e592f
Nikita Balyschew <n.balyschew@it-nik.de>
2022-01-04 11:53:32 +0100

    latest changes

--- pipeline/runTomoBear.m
+++ pipeline/runTomoBear.m
@@ -1,4 +1,7 @@
 function runTomoBear(compute_environment, configuration_path, default_configuration_path, starting_tomogram, ending_tomogram, step, gpu)
+if nargin <= 2
+    default_configuration_path = "configurations/defaults.json";
+end
 if nargin <= 3
     starting_tomogram = -1;
     ending_tomogram = -1;
