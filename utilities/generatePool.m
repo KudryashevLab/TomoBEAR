fa02c5a6c4bdb0e228bf51bf07d5230c98d1509a
Artsemi Yushkevich <artsemi.yushkevich@mdc-berlin.de>
2022-06-07 15:02:40 +0200

    Limited poolsize by used parcluster profile settings

--- utilities/generatePool.m
+++ utilities/generatePool.m
@@ -11,21 +11,26 @@ if string(path) ~= ""
 end
 
 if force || previous_cores == 0 || previous_cores ~= cores
+    % get number of cores available on the used machine
+    % and assigned to MATLAB
+    if cores <= 1
+        poolsize = round(feature('numcores') / cores);
+    else
+        poolsize = cores;
+    end
+    
+    % check whether required poolsize do not exceed number of workers
+    % allowed by used parcluster profile settings
+    if poolsize > pc.NumWorkers
+        poolsize = pc.NumWorkers;
+    end
+    
+    % open a pool
     if isempty(poolobj)
-        if cores <= 1
-            poolsize = round(feature('numcores') / (1/cores));
-        else
-            poolsize = cores;
-        end
         disp("INFO:poolsize: " + poolsize);
         poolobj = parpool(pc, poolsize);
     else
         current_poolsize = poolobj.NumWorkers;
-        if cores <= 1
-            poolsize = round(feature('numcores') / (1/cores));
-        else
-            poolsize = cores;
-        end
         if current_poolsize ~= poolsize
             disp("INFO:poolsize: " + poolsize);
             delete(poolobj);
