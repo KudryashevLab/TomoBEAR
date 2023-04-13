905fcadddc420cd7971aff12d798c943ddf0b6c6
Artsemi Yushkevich <Artsemi.Yushkevich@mdc-berlin.de>
2023-04-13 16:44:32 +0200

    add licensing info

--- utilities/template_matching_post_processing_iteration.m
+++ utilities/template_matching_post_processing_iteration.m
@@ -1,3 +1,22 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This file is part of the TomoBEAR software.
+% Copyright (c) 2021-2023 TomoBEAR Authors <https://github.com/KudryashevLab/TomoBEAR/blob/main/AUTHORS.md>
+% 
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Affero General Public License as
+% published by the Free Software Foundation, either version 3 of the
+% License, or (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU Affero General Public License for more details.
+% 
+% You should have received a copy of the GNU Affero General Public License
+% along with this program.  If not, see <https://www.gnu.org/licenses/>.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 function [tab_tomo, temporary_files] = template_matching_post_processing_iteration(configuration, binned_tomograms_paths_filtered, output_path, mask_erase)
 [folder, name, extension] = fileparts(binned_tomograms_paths_filtered.folder);
 name_splitted = strsplit(name, "_");
