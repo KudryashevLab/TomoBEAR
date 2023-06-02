bb5501eec1f8c59d67e232b2b587a9c0eda39fed
Artsemi Yushkevich <artsemi.yushkevich@mdc-berlin.de>
2023-06-02 16:36:41 +0200

    change license notice

--- utilities/getTiltStacks.m
+++ utilities/getTiltStacks.m
@@ -1,21 +1,20 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % This file is part of the TomoBEAR software.
-% Copyright (c) 2021-2023 TomoBEAR Authors <https://github.com/KudryashevLab/TomoBEAR/blob/main/AUTHORS.md>
+% Copyright (c) 2021,2022,2023 TomoBEAR Authors <https://github.com/KudryashevLab/TomoBEAR/blob/main/AUTHORS.md>
 % 
 % This program is free software: you can redistribute it and/or modify
-% it under the terms of the GNU Affero General Public License as
-% published by the Free Software Foundation, either version 3 of the
-% License, or (at your option) any later version.
+% it under the terms of the GNU General Public License as published
+% by the Free Software Foundation, either version 3 of the License,
+% or (at your option) any later version.
 % 
 % This program is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU Affero General Public License for more details.
+% GNU General Public License for more details.
 % 
-% You should have received a copy of the GNU Affero General Public License
+% You should have received a copy of the GNU General Public License
 % along with this program.  If not, see <https://www.gnu.org/licenses/>.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 
 function tilt_stacks = getTiltStacks(configuration, flatten)
 if nargin == 1
