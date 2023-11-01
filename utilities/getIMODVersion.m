cfd5ce22666b14ac3749ea48617d5389204f03d8
amineuron <67607590+amineuron@users.noreply.github.com>
2023-11-01 01:02:28 +0100

    Update getIMODVersion.m
    ctfphaseflib check the imod version which look into wrong line of help 3 instead of 2

--- utilities/getIMODVersion.m
+++ utilities/getIMODVersion.m
@@ -19,7 +19,7 @@
 function imod_version = getIMODVersion()
 output = executeCommand("3dmod -h", true, -1, true);
 output_lines = textscan(output,"%s","Delimiter","","endofline","\n");
-output_lines = output_lines{1}{2};
+output_lines = output_lines{1}{3};
 imod_version = regexp(output_lines, "\d+\.\d+\.\d+", "match");
 % TODO:NOTE everywhere available but what if folder is not named by version
 if string(getenv("IMOD_DIR")) ~= "" && length(imod_version) > 1 || isempty(imod_version)
