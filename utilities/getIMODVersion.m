6cae11ef12b1cfa299e23424361a70c33cb4e948
amineuron <67607590+amineuron@users.noreply.github.com>
2023-11-29 08:59:51 +0100

    Update getIMODVersion.m
    bring back

--- utilities/getIMODVersion.m
+++ utilities/getIMODVersion.m
@@ -21,11 +21,6 @@ output = executeCommand("3dmod -h", true, -1, true);
 output_lines = textscan(output,"%s","Delimiter","","endofline","\n");
 output_lines = output_lines{1}{2};
 imod_version = regexp(output_lines, "\d+\.\d+\.\d+", "match");
-if isempty(imod_version)
-    output_lines = textscan(output,"%s","Delimiter","","endofline","\n");
-    output_lines = output_lines{1}{3};
-    imod_version = regexp(output_lines, "\d+\.\d+\.\d+", "match");
-end
 % TODO:NOTE everywhere available but what if folder is not named by version
 if string(getenv("IMOD_DIR")) ~= "" && length(imod_version) > 1 || isempty(imod_version)
     imod_version = regexp(string(getenv("IMOD_DIR")), "[\d]+\.[\d]+\.[\d]+", "match");
