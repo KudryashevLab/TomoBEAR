764edf56060649a700be37638ac8495d71a66182
Artsemi Yushkevich <artsemi.yushkevich@mdc-berlin.de>
2023-11-30 16:58:00 +0100

    fix IMOD version checker
    Co-Authored-By: amineuron <amineuron@users.noreply.github.com>

--- utilities/getIMODVersion.m
+++ utilities/getIMODVersion.m
@@ -19,8 +19,13 @@
 function imod_version = getIMODVersion()
 output = executeCommand("3dmod -h", true, -1, true);
 output_lines = textscan(output,"%s","Delimiter","","endofline","\n");
-output_lines = output_lines{1}{2};
-imod_version = regexp(output_lines, "\d+\.\d+\.\d+", "match");
+for i=1:length(output_lines{1})
+    if contains(output_lines{1}{i}, 'Version')
+        version_line = output_lines{1}{i};
+        break;
+    end
+end
+imod_version = regexp(version_line, "\d+\.\d+\.\d+", "match");
 % TODO:NOTE everywhere available but what if folder is not named by version
 if string(getenv("IMOD_DIR")) ~= "" && length(imod_version) > 1 || isempty(imod_version)
     imod_version = regexp(string(getenv("IMOD_DIR")), "[\d]+\.[\d]+\.[\d]+", "match");
